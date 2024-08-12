689e0a2db59c649e2616f043cca0ba5292f8067f
mihneabondor <mihnea.bondor@icloud.com>
2024-08-12 20:31:32 +0300

    about us

--- aimacorp/src/Pages/AboutUs/AboutUs.js
+++ aimacorp/src/Pages/AboutUs/AboutUs.js
@@ -1,8 +1,9 @@
 import './AboutUs.css'
 
 import NavigationBar from '../../Components/NavigationBar/NavigationBar'
-import ThreeArrowTexts from '../../Components/ThreeByThreeArrowTexts/ThreeArrowTexts';
 import FourReviewStack from '../../Components/FourReviewStack/FourReviewStack';
+import CustomSutbtitle from '../../Components/CustomSubtitle/CustomSubtitle';
+import Footer from '../../Components/Footer/Footer';
 
 import {useNavigate} from 'react-router-dom'
 
@@ -16,45 +17,25 @@ function AboutUs() {
             <br/> <br/>
 
             <div className="aboutus-motivatie">
-                <img src='https://t5b34d.webwave.dev/lib/t5b34d/unsplash-image-lz01gpvz.jpg' className='aboutus-motivatie-image' alt=''></img>
                 <div className='aboutus-motivatie-texts'>
-                    <h4><b>My work is not a just a job, it's a passion</b></h4>
+                    <CustomSutbtitle
+                        title='WHO ARE WE?' />
                     <br/>
-                    <h2><b>Andrei Popescu</b></h2>
+                    <p>Aima Innovations provides technical consulting and CAD design for engineering projects. Our dedicated and talented team works to provide customized and innovative solutions, leading clients to achieving their goals and realize their ideas faster and more efficiently. We ensure that each project is carried out with your needs in mind, and a high attention to detail, guaranteeing the highest quality of our services for you. Contact us to find out how we can help you make your dreams come true!</p>
                     <br/>
-                    <p>I'm the proud owner of an engineering company that specializes in design and consultancy. With a considerable background in engineering and a strong passion for innovative solutions, I proudly lead a team of experimented experts. I am known for my dynamic approach and results-oriented mindset, always striving to provide the highest quality service to my clients. </p>
-                    <br/>
-                    <button className='aboutus-motivatie-texts-button' onClick={() => {nav('/contact')}}>Contact</button>
                 </div>
+                <img src='https://jqw818.webwave.dev/files/dynamicContent/sites/jqw818/images/ro/webpage_21/lzqv8tkl/element_702/rwdMode_1/588x347/1.webp' className='aboutus-motivatie-image' alt=''></img>
             </div>
 
             <br/> <br/>
 
-            <div className='cepotface'>
-                <h3 className='cepotface-title'><b>What can I do for you?</b></h3>
-                <div className='cepotface-arrowtexts-container'>
-                <ThreeArrowTexts
-                    text1='Engineering and consultancy'
-                    text2='Feasibility studies'
-                    text3='Project management'
-                />
+            <div className='cepotface'> </div>
 
-                <ThreeArrowTexts
-                    text1='Energetic efficency'
-                    text2='Industrial automatization'
-                    text3='Punctual consultancy'
-                />
-
-                <ThreeArrowTexts
-                    text1='Quality control'
-                    text2='Environment protection'
-                    text3='Technical support'
-                />
-                </div>
-            </div>
-
+            <br/> <br/>
             <div className='reviews'>
-                <h3 className='cepotface-title'><b>What does our clients think?</b></h3>
+                <CustomSutbtitle
+                    title="OUR CLIENTS' OPINION"
+                />
                 <FourReviewStack
                     title1='Tech Solutions Co.'
                     review1='We hired this engineering firm for a project and were very impressed with their professionalism and expertise. They delivered innovative solutions and were very attentive to details. We recommend with confidence!'
@@ -65,9 +46,11 @@ function AboutUs() {
                     title4='Futuretech Solutions'
                     review4='We have worked with many engineering firms in the past, but none have impressed us as much as this one. Their passion for each project, attention to detail and high quality service make them stand out. We highly recommend him to anyone in need of engineering services.'
                 />
+                <br/>
+                <br/>
             </div>
-            
 
+            <Footer/>
         </div>
     )
 }
